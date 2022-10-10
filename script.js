c8ab34975c959664b39af1fd480cf19f051b3b27
мансур зурабов <asl.zurabov@gmail.com>
2022-10-10 13:09:00 +0300

    making responsive at width 900 700 500pxs

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const container = document.querySelector('.container')
 menu.addEventListener('click', () => {
     container.classList.toggle('change')
 })
+
+// scroll smooth effect
+const scrollBtn = document.querySelector('.scroll-btn')
+const ht = document.querySelector('html')
+
+scrollBtn.addEventListener('click', () => {
+    ht.style.scrollBehavior = "smooth";
+    setTimeout(()=>{
+        ht.style.scrollBehavior = 'unset'
+    },1000)
+})
